a3877f147893aae3384f1b8af0f821cf179f195c
Matt Paul <mrpaul@g.cofc.edu>
2014-05-05 13:54:28 -0400

    Added the first script, creates a count table from RSEM ouputs

---
+++ counts_create.py
@@ -0,0 +1,66 @@
+import sys
+import csv
+import numpy
+import array
+import pandas as pd
+#Produced by Matt Paul, 5.5.2014
+#This tool converts the expected counts from gene.results file outputs from RSEM and creates a counts table to be used for DESeq2 and edgeR. Create a folder with all of the RSEM outputs in them (sampleName.gene.results) and run this command
+    "python counts_create.py /filepath_to_outputs/*.gene.results"
+
+#Gets the name of the first file in list of arguments
+name = sys.argv[1][:-20]
+#opens first file
+firstFile = open(sys.argv[1], 'rb')
+#reads the file as a csv
+firstData = csv.reader(firstFile, delimiter='\t')
+
+#Create 2 arrays that serve as the first collumn in the table
+gene_ids = []
+first_counts = []
+
+#This block populates the first two collumns (arrays), had a i=0 case which is equivalent to the header
+i = 0
+for line in firstData:
+    if i != 0:
+        gene_ids.append(line[0])
+        first_counts.append(int(round(float(line[4]), 0)))
+    else:
+        gene_ids.append("gene_id")
+        first_counts.append(name)
+    i += 1
+
+#Created a two-dimensional array that contains the first collumns (arrays)
+counts_table= [gene_ids, first_counts]
+
+#Now that there is a 2D array with the gene_ids and the first sample's expected counts, this block creates and populates and array of exp. counts from each sample then adds it to the 2D array, effectively adding a new column
+for i in range(2, len(sys.argv)):
+    name = sys.argv[i][:-20]
+    file = open(sys.argv[i], 'rb')
+    data = csv.reader(file, delimiter='\t')
+    counts=[]
+
+    m = 0
+    for line in data:
+        if m != 0:
+            counts.append(int(round(float(line[4]), 0)))
+        else:
+            counts.append(name)
+            m += 1
+
+    counts_table.append(counts)
+
+#Make the 2D array into a numpy array (maybe not necessary?)
+ar = numpy.array(counts_table)
+#Create a new csv file and initiate a csv writer for that file
+output = open('shrimp.counts.csv', 'w')
+writer = csv.writer(output)
+
+#The for loop iterates though every element in the 2D arrays via rows (i). The collumns are accessed in the array (x). Therefore, x[i] is the expected count value for sample x.
+for i in range(0, len(gene_ids)-1):
+    row = []
+    for x in ar:
+        row.append(x[i])
+    writer.writerow(row)
+output.close()
+
+print "well it ran"
